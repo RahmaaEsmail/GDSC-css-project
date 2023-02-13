f3b578a7849b676391cd4657e0402549a05e3767
RahmaaEsmail <108898486+RahmaaEsmail@users.noreply.github.com>
2023-02-13 15:58:25 +0200

    update sticky nav

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-const menuIcon = document.querySelector(".menu-icon");
-const header = document.querySelector(".header")
-menuIcon.addEventListener("click",()=>{
-    menuIcon.classList.toggle("open-nav");
-    header.classList.toggle("openlist")
-})
-
-
 const allLinks = document.querySelectorAll(".nav--list a:link");
-allLinks.forEach(function(link){
-    link.addEventListener("click",(e)=>{
+allLinks.forEach(function (link) {
+    link.addEventListener("click", (e) => {
         e.preventDefault();
         const href = link.getAttribute("href");
         console.log(href);
-        if(href!=="#" && href.startsWith("#")) {
+        if (href !== "#" && href.startsWith("#")) {
             const section = document.querySelector(href);
-            section.scrollIntoView({behavior:"smooth"});
+            section.scrollIntoView({ behavior: "smooth" });
         }
 
     })
 })
 
+const menuIcon = document.querySelector(".menu-icon");
+const header = document.querySelector(".header")
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("open-nav");
+    header.classList.toggle("openlist");
+})
+
+
 
 window.onscroll = function () {
-     myFunction() };
-
-const navbar = document.querySelector(".header");
-const sticky = navbar.offsetTop;
-console.log(sticky);
+    myFunction()
+};
 
 function myFunction() {
     if (window.pageYOffset >= 100) {
-        navbar.classList.add("sticky")
+        header.classList.add("sticky")
     } else {
-        navbar.classList.remove("sticky");
+        header.classList.remove("sticky");
     }
 }
+
+
+
+
